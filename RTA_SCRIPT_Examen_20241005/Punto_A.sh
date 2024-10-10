f16d1cc544cc1ae10c9b8a4a405ba5dcc3fdf426
aguslo03 <150861381+aguslo03@users.noreply.github.com>
2024-10-09 21:40:26 -0300

    Delete RTA_SCRIPT_Examen_20241005 directory

--- RTA_SCRIPT_Examen_20241005/Punto_A.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "creo estructura Punto A"
-
-sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
-
-echo
-echo "muestro estructura Punto A"
-sudo tree /Examenes-UTN
